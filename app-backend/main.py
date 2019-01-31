3b4d711162dc141db0b9d98fecc9bdf6c174252b
Adrian Cieluch <adrian.cieluch@mayflower.de>
2019-01-31 14:56:30 +0100

    Added project creation stub

--- app-backend/main.py
+++ app-backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from werkzeug.wrappers import Response
 
 app = Flask(__name__)
 
@@ -6,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def health_check():
     return 'alive'
+
+
+@app.route('/projects', methods=['POST'])
+def projects():
+    return Response(status='201 Created', headers={'Location': '/project/1'})
 
 
 if __name__ == '__main__':
--- app-backend/test.py
+++ app-backend/test.py
@@ -11,5 +11,18 @@ class BasicTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
 
+class ProjectCreationTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.tester = app.test_client(self)
+
+    def test_create_simple_project(self):
+        response = self.tester.post('/projects', json={
+            'name': 'Simple Project', 'description': 'This is a simple sample project.'
+        })
+        self.assertEqual(response.status_code, 201)
+        self.assertIn(member='Location', container=response.headers)
+
+
 if __name__ == '__main__':
     unittest.main()
