bf526fd0048ac9dbb6c328a30dd5e390b2bf77ef
Adrian Cieluch <adrian.cieluch@mayflower.de>
2019-02-01 11:18:46 +0100

    Added migrations

--- app-backend/app/__init__.py
+++ app-backend/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 import os
 
 app_object = Flask(__name__)
 app_object.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}/{os.environ['POSTGRES_DB']}"
 db = SQLAlchemy(app_object)
+migrate = Migrate(app_object, db)
 
 from app.project import project_api
 
