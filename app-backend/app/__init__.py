f001dc79362fbcf00178370df510b7ba31dfa20f
Adrian Cieluch <adrian.cieluch@mayflower.de>
2019-02-01 15:44:54 +0100

    Added CORS

--- app-backend/app/__init__.py
+++ app-backend/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 import os
 
@@ -8,6 +9,7 @@ app_object = Flask(__name__)
 app_object.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}/{os.environ['POSTGRES_DB']}"
 db = SQLAlchemy(app_object)
 migrate = Migrate(app_object, db)
+cors = CORS(app_object, resources={r"/*": {"origins": "*"}})
 
 from app.project import project_api
 
