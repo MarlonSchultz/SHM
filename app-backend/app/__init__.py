b0601d71f2c29b3abd2c7fd6eb22bbb99ce96a6a
Adrian Cieluch <adrian.cieluch@mayflower.de>
2019-02-01 11:03:57 +0100

    Added database config from environment variables

--- app-backend/app/__init__.py
+++ app-backend/app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+import os
+
 app_object = Flask(__name__)
-#app_object.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app_object.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://test:password@postgres/example'
+app_object.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}/{os.environ['POSTGRES_DB']}"
 db = SQLAlchemy(app_object)
 
 from app.project import project_api
--- app-backend/debug_server.sh
+++ app-backend/debug_server.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+source ../docker/postgres/database.conf
+export POSTGRES_USER
+export POSTGRES_PASSWORD
+export POSTGRES_HOST
+export POSTGRES_PORT
+export POSTGRES_DB
+
 python3 main.py
