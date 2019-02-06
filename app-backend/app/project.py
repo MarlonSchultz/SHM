916937907d5b0d0c1c37eb2a97c79e6748c0a2e4
Adrian Cieluch <adrian.cieluch@mayflower.de>
2019-02-06 09:38:38 +0100

    Added comments

--- app-backend/app/project.py
+++ app-backend/app/project.py
@@ -5,15 +5,30 @@ from app import db
 
 
 class Project(db.Model):
+    """
+    Project class.
+    """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(300), unique=False, nullable=False)
     description = db.Column(db.Text(), unique=False, nullable=True)
 
     def to_json(self):
+        """
+        Turn class fields into json.
+
+        :return: The json formatted fields.
+        """
         return {'id': self.id, 'name': self.name, 'description': self.description}
 
 
 def create_project(name: str, description: str) -> Project:
+    """
+    Create a project and add it to the database.
+
+    :param name: The project name.
+    :param description: The project description.
+    :return: The created project.
+    """
     project = Project(name=name, description=description)
     db.session.add(project)
     db.session.commit()
@@ -22,6 +37,11 @@ def create_project(name: str, description: str) -> Project:
 
 
 def list_projects() -> []:
+    """
+    Fetch and return a list of all existing projects.
+
+    :return: The fetched project list.
+    """
     project_list = Project.query.all()
     return project_list
 
@@ -31,6 +51,11 @@ project_api = Blueprint('Project API', __name__)
 
 @project_api.route('/projects', methods=['POST'])
 def post_projects():
+    """
+    Provide a POST API endpoint for creating projects.
+
+    :return: The response for creating a project.
+    """
     json_data = request.get_json()
     project = create_project(name=json_data['name'], description=json_data['description'])
 
@@ -39,6 +64,11 @@ def post_projects():
 
 @project_api.route('/projects', methods=['GET'])
 def get_projects():
+    """
+    Provide a GET API endpoint for retrieving projects.
+
+    :return: The json-formatted project-list.
+    """
     project_list = list_projects()
     json_project_list = [project.to_json() for project in project_list]
     return jsonify(json_project_list)
