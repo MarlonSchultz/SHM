eed5c08636143096e490905fbc3ef054880417c9
Martin Holzhauer <martin@holzhauer.eu>
2019-02-20 14:39:24 +0100

    added setupTests to setup the react adapter

--- app-frontend/src/components/Projects/ProjectInput/ProjectInput.test.tsx
+++ app-frontend/src/components/Projects/ProjectInput/ProjectInput.test.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
 import ProjectInput from './ProjectInput';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
     const container = shallow(<ProjectInput onSave={() => {}} />);
