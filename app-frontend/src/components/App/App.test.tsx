4e4eee322c34531bf5259ff08bb72f81dadc0d2f
Martin Holzhauer <martin@holzhauer.eu>
2019-02-20 14:41:16 +0100

    cleaned up the App test

--- app-frontend/src/components/App/App.test.tsx
+++ app-frontend/src/components/App/App.test.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme, {shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
 import App from './App';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const container = shallow(<App/>);
