93e3b8ff3cbffa325c8654817ece952e46806a46
Martin Holzhauer <martin@holzhauer.eu>
2019-02-21 09:35:34 +0100

    more testing for ProjectInput component

--- app-frontend/src/components/Projects/ProjectInput/ProjectInput.test.tsx
+++ app-frontend/src/components/Projects/ProjectInput/ProjectInput.test.tsx
@@ -18,6 +18,28 @@ it('Button has right text', () => {
     expect(container.find('textarea').length).toBe(1);
 });
 
+it('input and clicked', () => {
+    let callback = jest.fn();
+
+    const container = shallow(<ProjectInput onSave={callback} />);
+    
+    let title = container.find('input');
+    title.simulate('change', { target: { name: 'name', value: 'Titletext' } } );
+    
+    let text = container.find('textarea');
+    text.simulate('change', { target: { name: 'description', value: 'Text' } } );
+
+    expect(container.state().name).toBe('Titletext');
+    expect(container.state().description).toBe('Text');
+
+    container.find('button').simulate('click');
+    
+    expect(callback.mock.calls.length).toBe(1);
+
+    expect(callback.mock.calls[0][0]).toBe('Titletext');
+    expect(callback.mock.calls[0][1]).toBe('Text');
+});
+
 it('clicked', () => {
     let callback = jest.fn();
 
