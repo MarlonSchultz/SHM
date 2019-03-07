c0c8e616d79774f196271640bb6be97a0b968d3e
Adrian Cieluch <adrian.cieluch@mayflower.de>
2019-03-07 16:22:42 +0100

    Added stakeholder list retrieval endpoint & tests

--- app-backend/app/stakeholder.py
+++ app-backend/app/stakeholder.py
@@ -36,11 +36,21 @@ def create_stakeholder(project_id: int, name: str, company: str, role: str, atti
     :param attitude: Attitude of the stakeholder.
     :return: The created project.
     """
-    stakeholder = Stakeholder(project_id = project_id, name = name, company = company, role = role, attitude = attitude)
+    stakeholder = Stakeholder(project_id=project_id, name=name, company=company, role=role, attitude=attitude)
     db.session.add(stakeholder)
     db.session.commit()
 
     return stakeholder
+
+
+def list_stakeholders(project_id: int) -> []:
+    """
+    Fetch and return a list of all existing projects.
+
+    :return: The fetched project list.
+    """
+    stakeholder_list = Stakeholder.query.filter_by(project_id=project_id).all()
+    return stakeholder_list
 
 
 stakeholder_api = Blueprint('Stakeholder API', __name__)
@@ -48,16 +58,17 @@ stakeholder_api = Blueprint('Stakeholder API', __name__)
 
 @stakeholder_api.route('/project/<project_id>/stakeholder', methods=['POST'])
 def post_stakeholder(project_id: int):
-    
     try:
         json_data = request.get_json()
         company = json_data['company'] if 'company' in json_data else None
         role = json_data['role'] if 'role' in json_data else None
         attitude = json_data['attitude'] if 'attitude' in json_data else None
 
-        stakeholder = create_stakeholder(project_id=project_id, name=json_data['name'], company=company, role=role, attitude=attitude)
+        stakeholder = create_stakeholder(project_id=project_id, name=json_data['name'], company=company, role=role,
+                                         attitude=attitude)
 
-        return Response(status='201 Created', headers={'Location': f'/project/{stakeholder.project_id}/stakeholder/{stakeholder.id}'})
+        return Response(status='201 Created',
+                        headers={'Location': f'/project/{stakeholder.project_id}/stakeholder/{stakeholder.id}'})
     except KeyError as error:
         return Response(response=f"Missing data: {', '.join(error.args)}", status='422 Unprocessable Entity')
     except TypeError as error:
@@ -66,3 +77,15 @@ def post_stakeholder(project_id: int):
         return jsonify('Could not find stakeholder'), 500
     except ProgrammingError:  # Table not found
         return jsonify('Could not find stakeholder'), 500
+
+
+@stakeholder_api.route('/project/<project_id>/stakeholder', methods=['GET'])
+def get_stakeholder_list(project_id: int):
+    try:
+        stakeholder_list = list_stakeholders(project_id)
+        json_stakeholder_list = [stakeholder.to_json() for stakeholder in stakeholder_list]
+        return jsonify(json_stakeholder_list)
+    except OperationalError:  # Could not connect to database
+        return jsonify('Could not find stakeholders'), 500
+    except ProgrammingError:  # Table not found
+        return jsonify('Could not find stakeholders'), 500
--- app-backend/tests/test_stakeholder.py
+++ app-backend/tests/test_stakeholder.py
@@ -7,6 +7,15 @@ import app
 import unittest
 
 
+class MockDatabaseModelResult:
+
+    def __init__(self):
+        self.results = []
+
+    def all(self):
+        return self.results
+
+
 def _db_session_add_side_effect(project):
     project.id = 1
     return project
@@ -128,6 +137,64 @@ class StakeholderCreationTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 500)
         self.assertEqual(response.is_json, True)
         self.assertEqual(response.json, 'Could not find stakeholder')
+
+
+class StakeHolderListRetrievalTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.tester = app_object.test_client(self)
+        self.mock_stakeholder = app.stakeholder.Stakeholder(id=1, project_id=1, name="Stakeholder 1",
+                                                            company="Wichtig GmbH",
+                                                            role="Chief of Wichtigkeit",
+                                                            attitude="alles muss man selber machen")
+
+    @mock.patch("app.stakeholder.Stakeholder")
+    def test_retrieve_stakeholder_list(self, mock_stakeholder_model):
+        mock_stakeholder_result = MockDatabaseModelResult()
+        mock_stakeholder_result.results = [self.mock_stakeholder]
+
+        mock_stakeholder_model.query.filter_by.return_value = mock_stakeholder_result
+        stakeholder_list = app.stakeholder.list_stakeholders(1)
+
+        self.assertEqual(len(stakeholder_list), 1)
+        self.assertEqual(stakeholder_list, [self.mock_stakeholder])
+
+    @mock.patch("app.stakeholder.Stakeholder")
+    def test_retrieve_stakeholder_list_empty(self, mock_stakeholder_model):
+        mock_stakeholder_result = MockDatabaseModelResult()
+        mock_stakeholder_result.results = []
+
+        mock_stakeholder_model.query.filter_by.return_value = mock_stakeholder_result
+        stakeholder_list = app.stakeholder.list_stakeholders(100000)
+
+        self.assertEqual(stakeholder_list, [])
+
+    @mock.patch("app.stakeholder.list_stakeholders")
+    def test_retrieve_stakeholder_list_endpoint(self, mock_list_stakeholders):
+        mock_list_stakeholders.return_value = [self.mock_stakeholder]
+        response = self.tester.get('/project/1/stakeholder', content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.is_json, True)
+        self.assertEqual(response.json, [self.mock_stakeholder.to_json()])
+
+    @mock.patch("app.stakeholder.list_stakeholders")
+    def test_retrieve_stakeholder_list_endpoint_no_database(self, mock_list_stakeholders):
+        mock_list_stakeholders.side_effect = _raise_operational_error
+        response = self.tester.get('/project/1/stakeholder', content_type='application/json')
+
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.is_json, True)
+        self.assertEqual(response.json, 'Could not find stakeholders')
+
+    @mock.patch("app.stakeholder.list_stakeholders")
+    def test_retrieve_stakeholder_list_endpoint_no_table(self, mock_list_stakeholders):
+        mock_list_stakeholders.side_effect = _raise_programming_error
+        response = self.tester.get('/project/1/stakeholder', content_type='application/json')
+
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.is_json, True)
+        self.assertEqual(response.json, 'Could not find stakeholders')
 
 
 if __name__ == '__main__':
